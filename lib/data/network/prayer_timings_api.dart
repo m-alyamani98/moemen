491887a9fdd09484ceaff2a5953e75ca6b13187c
Malak Alyamani <65876164+m-alyamani98@users.noreply.github.com>
2025-07-22 23:52:11 +0300

    Update prayer_timings_api.dart

--- lib/data/network/prayer_timings_api.dart
+++ lib/data/network/prayer_timings_api.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:retrofit/error_logger.dart';
 
 import 'package:retrofit/http.dart';
 
@@ -11,12 +10,12 @@ part 'prayer_timings_api.g.dart';
 @RestApi(baseUrl: Constants.baseUrl)
 abstract class PrayerTimingsServiceClient {
   factory PrayerTimingsServiceClient(Dio dio, {String baseUrl}) =
-      _PrayerTimingsServiceClient;
+  _PrayerTimingsServiceClient;
 
   @GET("{date}?city={city}&country={country}&school=0&method=5")
   Future<PrayerTimingsResponse> getPrayerTimings(
-    @Path("date") String date,
-    @Path("city") String city,
-    @Path("country") String country,
-  );
+      @Path("date") String date,
+      @Path("city") String city,
+      @Path("country") String country,
+      );
 }
